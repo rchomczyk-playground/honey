5119a34c0e368bf76c54e725127fcd83ab7da919
Damian <damian.festowski@onet.pl>
2024-11-16 19:20:29 +0100

    impl kt extension #1

---
+++ honey-kt-extension/src/dev/shiza/honey/title.kt
@@ -0,0 +1,73 @@
+import dev.shiza.honey.dispatcher.TitleMessageDispatcher
+import dev.shiza.honey.dispatcher.MessageDispatcher
+import dev.shiza.honey.message.Message
+import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.Component
+
+typealias TitleConfigurer = TitleMessageDispatcher<Audience, Message>.() -> Unit
+typealias MessageConfigurer = MessageDispatcher<Audience, Message>.() -> Unit
+
+fun times(fadeIn: Int, stay: Int, fadeOut: Int): Triple<Int, Int, Int> = 
+    Triple(fadeIn, stay, fadeOut)
+
+
+fun TitleMessageDispatcher.createTitle(
+    recipient: Audience,
+    titleConfig: MessageConfigurer,
+    subtitleConfig: MessageConfigurer,
+    fadeIn: Int,
+    stay: Int,
+    fadeOut: Int
+): MessageDispatcher<Audience, Message> =
+    createTitle()
+        .recipient(recipient)
+        .title { base -> base.apply(titleConfig) }
+        .subtitle { base -> base.apply(subtitleConfig) }
+        .times(fadeIn, stay, fadeOut)
+
+fun Audience.createTitle(
+    titleConfig: MessageConfigurer,
+    subtitleConfig: MessageConfigurer,
+    fadeIn: Int,
+    stay: Int,
+    fadeOut: Int
+): TitleMessageDispatcher<Audience, Message> =
+    createTitle()
+        .recipient(this)
+        .title { base -> base.apply(titleConfig) }
+        .subtitle { base -> base.apply(subtitleConfig) }
+        .times(fadeIn, stay, fadeOut)
+
+fun Player.createTitle(
+    titleConfig: MessageConfigurer,
+    subtitleConfig: MessageConfigurer,
+    fadeIn: Int,
+    stay: Int,
+    fadeOut: Int
+): TitleMessageDispatcher<Audience, Message> =
+    createTitle()
+        .recipient(this)
+        .title { base -> base.apply(titleConfig) }
+        .subtitle { base -> base.apply(subtitleConfig) }
+        .times(fadeIn, stay, fadeOut)
+
+
+fun TitleConfigurer.withFormattedTitle(messageFormatter: MessageFormatter): TitleMessageDispatcher<Audience, Message> =
+    this.title { base -> 
+        base.apply { messageFormatter.format(this) }
+    }
+
+fun Audience.createTitle(
+    messageFormatter: MessageFormatter<Component>,
+    titleConfig: MessageConfigurer,
+    subtitleConfig: MessageConfigurer,
+    fadeIn: Int,
+    stay: Int,
+    fadeOut: Int
+): TitleMessageDispatcher<Audience, Message> =
+    createTitle()
+        .recipient(this)
+        .withFormattedTitle(messageFormatter) 
+        .title { base -> base.apply(titleConfig) }
+        .subtitle { base -> base.apply(subtitleConfig) }
+        .times(fadeIn, stay, fadeOut)
