0b616370e4b575ab0a799aa6a0ddf3ba3ffd4e1d
rafal <63915083+rchomczyk@users.noreply.github.com>
2024-11-16 22:58:13 +0100

    Add kotlin extension (6)

--- honey-kt-extension/build.gradle.kts
+++ honey-kt-extension/build.gradle.kts
@@ -5,6 +5,15 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(project(":honey-common"))
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
 honeyPublish {
     artifactId = "honey-kt-extension"
 }
